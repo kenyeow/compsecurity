b0a516340e89808405e7b28785f1f0f388da92bf
theflybirdstudio <ken_gaming@hotmail.com>
2019-01-25 15:59:48 +0800

    done addictive cipher

--- src/AdditiveCipher.java
+++ src/AdditiveCipher.java
@@ -1,24 +1,99 @@
 public class AdditiveCipher {
 
     static String alphabetString = "abcdefghijklmnopqrstuvwxyz";
-    static char[] alphabet = alphabetString.toCharArray();
 
     /**
-     * Additive Cipher Encryption Algorithm
+     * Additive Cipher Encryption Algorithm (no plaintext checking)
      * @param plaintext
      * @param key
      * **/
     public static void encryption(String plaintext, Integer key) {
-        String tempCipherText;
+        if(checkKey(key) == false){
+            System.out.println("Invalid Key");
+            return;
+        }
+
+        String tempCipher = "";
+        String tempCipherText = "";
 
         // remove spaces
         plaintext = plaintext.replaceAll("\\s+","");
 
         for(int i = 0; i < plaintext.length(); i++){
             char p = plaintext.charAt(i);
+
+            // get index of each character in the string
             int index = alphabetString.indexOf(p);
 
-            System.out.println("plaintext decimal :" + index);
+            // adding key + alphabet index
+            int temp = index + key;
+
+            // modulus operation
+            int cipher = temp % 26;
+
+            // push all cipher into temporary string
+            tempCipher += " " + cipher;
+
+            // convert cipher into alphabet (ciphertext)
+            char cipherText = alphabetString.charAt(cipher);
+
+            // push cipher text into temporary string
+            tempCipherText += " " + cipherText;
         }
+
+        // print out cipher + ciphertext
+        System.out.println("Cipher: " + tempCipher);
+        System.out.println("Cipher Text: " + tempCipherText);
+    }
+
+    /**
+     * Additive Cipher Decryption Algorithm (no ciphertext checking)
+     * @param ciphertext
+     * @param key
+     * **/
+    public static void decryption(String ciphertext, Integer key) {
+        if(checkKey(key) == false){
+            System.out.println("Invalid Key");
+            return;
+        }
+
+        String tempPlain = "";
+        String tempPlainText = "";
+
+        // remove spaces
+        ciphertext = ciphertext.replaceAll("\\s+","");
+
+        for(int i = 0; i < ciphertext.length(); i++){
+            char c = ciphertext.charAt(i);
+
+            // get index of each character of the string
+            int index = alphabetString.indexOf(c);
+
+            // subtract key and index
+            int temp = index - key;
+
+            // modulus operation
+            int plain = (((temp % 26) + 26) % 26);
+
+            // push all plain into temporary plain decimal
+            tempPlain += " " + plain;
+
+            // covert plain decimal into alphabet (plaintext)
+            char plaintext = alphabetString.charAt(plain);
+
+            // push all plaintext into temporary string
+            tempPlainText += plaintext;
+        }
+
+        System.out.println("Plaintext Decimal: " + tempPlain);
+        System.out.println("Plaintext: " + tempPlainText);
+    }
+
+    private static boolean checkKey(Integer key){
+        if(key > 25 || key < 1){
+            return false;
+        }
+
+        return true;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.Math.abs;
+
 public class Main {
 
     public static void main(String[] args){
@@ -19,13 +21,73 @@ public class Main {
                 while(addCipherSelection != 0){
                     if(addCipherSelection == 1){
                         Scanner scanner = new Scanner(System.in);
+
+                        // get plaintext input
                         System.out.print("Please enter the plaintext: ");
                         String plaintext = scanner.nextLine();
+
+                        // get encryption key
                         System.out.print("Please enter the encryption key: ");
                         Integer key = scanner.nextInt();
+
+                        // start time for time complexity calculation
+                        long startTime = System.currentTimeMillis();
+
+                        // additive cipher encryption algorithm
                         AdditiveCipher.encryption(plaintext, key);
+
+                        // stop time for time complexity calculation
+                        long stopTime = System.currentTimeMillis();
+
+                        // print out the result of time complexity
+                        System.out.println("Time complexity: " + (stopTime - startTime) + "ms\n");
+
+                        // covert plaintext length to double
+                        double plaintextLength = new Double(plaintext.length());
+
+                        // calculate byte
+                        double bytes =  Math.ceil(plaintextLength / 8);
+
+                        // calculate throughput -> bytes / executed time
+                        double throughput = bytes / (stopTime - startTime);
+
+                        // print throughput
+                        System.out.println("Throughput: " + throughput);
                     } else if (addCipherSelection == 2){
-                        System.out.println("Decryption");
+
+                        Scanner scanner = new Scanner(System.in);
+
+                        // get ciphertext input
+                        System.out.print("Please enter the ciphertext: ");
+                        String ciphertext = scanner.nextLine();
+
+                        // get decryption key
+                        System.out.print("Please enter the decryption key: ");
+                        Integer key = scanner.nextInt();
+
+                        // start time for time complexity calculation in ms.
+                        long startTime = System.currentTimeMillis();
+
+                        // additive cipher decryption algorithm
+                        AdditiveCipher.decryption(ciphertext, key);
+
+                        // stop time for time complexity calculation
+                        long stopTime = System.currentTimeMillis();
+
+                        // print out complexity result
+                        System.out.println("Time complexity: " + (stopTime - startTime) + "ms\n");
+
+                        // covert ciphertext length to double
+                        double ciphertextLength = new Double(ciphertext.length());
+
+                        // covert to bytes
+                        double bytes =  Math.ceil(ciphertextLength / 8);
+
+                        // calculate throughput -> bytes/time executed
+                        double throughput = bytes / (stopTime - startTime);
+
+                        // print throughput
+                        System.out.println("Throughput: " + throughput);
                     } else {
                         System.out.println("Wrong Input, Try again.");
                     }
